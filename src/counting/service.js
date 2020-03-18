c53536f511e8f7e4925908df1a1da57c00c89580
sergienko <vik.sergienko@mail.ru>
2020-03-18 20:36:00 +0300

    Fixed service

--- src/counting/service.js
+++ src/counting/service.js
@@ -5,23 +5,24 @@ const exchangeRatesApi = 'https://www.cbr-xml-daily.ru/daily_json.js';
 export const getTheExchangeRates = async () => {
   try {
     const { data } = await axios.get(exchangeRatesApi);
-    return { ...data, RUB: { Value: 1 } };
+    const { Valute } = data;
+    return { ...Valute, RUB: { Value: 1 } };
   } catch (e) {
     throw new Error(e.message);
   }
 };
 
-export const getCartCountingInRubles = (cart, exchange) => cart.reduce((acc, item) => {
+export const getCartCountingInRubles = (cart, exchanges) => cart.reduce((acc, item) => {
   const { quantity, price, currency } = item;
-  if (!(currency in exchange)) {
+  if (!(currency in exchanges)) {
     throw new Error(`invalid currency ${currency}`);
   }
-  const { Value } = exchange[currency];
+  const { Value } = exchanges[currency];
   return acc + (quantity * price * Value);
 }, 0);
 
-export const getConvertCurrency = (sum, exchange, currencies = ['RUB', 'EUR', 'USD']) => currencies.reduce((acc, currency) => {
-  const { Value } = exchange[currency];
+export const getConvertCurrency = (sum, exchanges, currencies = ['RUB', 'EUR', 'USD']) => currencies.reduce((acc, currency) => {
+  const { Value } = exchanges[currency];
   const convert = Number((sum / Value).toFixed(2));
   return { ...acc, [currency]: convert };
 }, {});
