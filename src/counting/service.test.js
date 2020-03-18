057739fb336653fc1d34f50a69092a2022167ed7
sergienko <vik.sergienko@mail.ru>
2020-03-18 16:03:09 +0300

    Added unit tests

--- src/counting/service.test.js
+++ src/counting/service.test.js
@@ -1,4 +1,6 @@
-import { getCartCountingInRubles } from './service';
+import { getCartCountingInRubles, getConvertCurrency } from './service';
+
+const exchange = { RUB: { Value: 1 }, EUR: { Value: 85 }, USD: { Value: 75 } };
 
 describe('get cart counting in rubles', () => {
   test('should return cart sum', async () => {
@@ -16,7 +18,25 @@ describe('get cart counting in rubles', () => {
         currency: 'EUR',
       },
     ];
-    const exchange = { RUB: { Value: 1 }, EUR: { Value: 85 }, USD: { Value: 75 } };
     expect(getCartCountingInRubles(cart, exchange)).toBe(2910);
+  });
+  test('should return error', async () => {
+    const cart = [
+      {
+        name: 'Milk',
+        quantity: 1,
+        price: 20,
+        currency: 'DDD',
+      },
+    ];
+    expect(() => getCartCountingInRubles(cart, exchange)).toThrow('invalid currency DDD');
+  });
+});
+
+describe('get convert currency', () => {
+  test('should return convert', async () => {
+    const sum = 2910;
+    const convertResult = { EUR: 34.24, RUB: 2910.00, USD: 38.80 };
+    expect(getConvertCurrency(sum, exchange)).toEqual(convertResult);
   });
 });
