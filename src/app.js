3cdd3f379f054fe9029769658f8eed2b34fa4a3d
sergienko <vik.sergienko@mail.ru>
2020-03-19 18:06:40 +0300

    Updated app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import countingController from './counting/controller';
 import { errorHandler } from './middleware';
 
@@ -9,7 +10,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
